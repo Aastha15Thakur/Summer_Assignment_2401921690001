c7099145d677c0ab9f3f4654d0396516058fade6
aasthacodes <glbaastha1506@gmail.com>
2026-06-14 15:12:49 +0530

    week 2 oops solution

---
+++ Week_2/OOPS/Week2_Interface_Library/src/live/Test.java
@@ -0,0 +1,20 @@
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+    public static void main(String[] args){
+
+        Veena veena= new Veena();
+        veena.play();
+        Saxophone saxophone = new Saxophone();
+        saxophone.play();
+
+        Playable p1= new Veena();
+        Playable p2= new Saxophone();
+        p1.play();
+        p2.play();
+    }
+}
