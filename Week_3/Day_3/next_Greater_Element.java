7ef0cd02174b5c771296e82de762b8ef71652359
aasthacodes <glbaastha1506@gmail.com>
2026-06-17 22:57:54 +0530

    week 3 day 3 leetcode solutions

---
+++ Week_3/Day_3/next_Greater_Element.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            int curr = nums2[i];
+
+            while (!stack.isEmpty() && stack.peek() < curr) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                map.put(curr, -1);
+            } else {
+                map.put(curr, stack.peek());
+            }
+            stack.push(curr);
+        }
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
+    }
+}
